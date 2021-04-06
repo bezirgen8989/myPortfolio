c47f341ac474e094963cc12c089eacbe4569f014
bezirgen8989 <bezirgen8989@mail.ru>
2021-04-06 19:22:57 +0300

    _pulick_url

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import {BrowserRouter} from "react-router-dom";
 import reduxStore from "./redux/reduxStore";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App state={reduxStore.getState()} reduxStore={reduxStore}/>
     </BrowserRouter>, document.getElementById('root'));
